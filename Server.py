da1ab4fb9d8956ed542430e7b0ddb63b48c4ca9d
NivTurk <nivturk@gmail.com>
2023-09-10 09:12:47 -0400

    server

---
+++ Server.py
@@ -0,0 +1,30 @@
+import socket as sok
+import threading  as trd
+
+sock = sok.socket(sok.AF_INET,sok.SOCK_STREAM)
+
+HOST = ('0.0.0.0', 10000)
+
+sock.listen(1)
+connections = []
+def handle_connections(c, a):
+    while True:
+        global connections
+        data = c.recv(1024)
+        for connection in connections:
+            connection.send(bytes(data))
+        if not data:
+            connections.remove(c)
+            c.close()
+            break
+
+
+while True:
+    c, a = sock.accept() 
+    print("here")
+    cThread = trd.Thread(target = handle_connections, args = (c , a))
+    cThread.daemon=True
+    cThread.start()
+    connections.append(c)
+    print(connections)
+
