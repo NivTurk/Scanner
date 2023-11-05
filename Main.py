70f6b5bcef474e80569de6b9f66b91e08a45e858
nivturk <nivturk@gmail.com>
2023-11-05 17:36:42 -0500

    added str anr repr to OpenPort class

--- Main.py
+++ Main.py
@@ -6,13 +6,18 @@ import re
 
 
 class OpenPort () :
-    def __init__  (self):
-        self.ip = "-1"
-        self.port = "-1"
-        
+    def __init__  (self, ip_val, port_val):
+        self.ip = ip_val
+        self.port = port_val
+
+    def __str__(self):
+        return str("ip: " + str(self.ip)+" port: "+str(self.port))
+    def __repr__(self):
+        return str(self)    
     def setip (self,addr):
         if isip(addr):
             self.ip = addr
+    
     def getip (self):
         return self.ip
     def setport (self,num):
@@ -20,6 +25,12 @@ class OpenPort () :
             self.port = num
     def getport(self):
         return self.port
+    def _ip_and_port(self):
+        str = self.getip()
+        str+= ','
+        str += self.getport()
+        return str
+    
         
         
         
@@ -53,15 +64,22 @@ def file_handle(tmp_file):
     return list
     
     
-    
+tst = OpenPort("addr" , "port")
+print("test ->" )
+print(tst)
+
 tmp_file = tempfile.NamedTemporaryFile()
 os.system( 'netstat -a |awk \'{print $5}\'> %s' % tmp_file.name)
 print(tmp_file.name) #file location
 content = tmp_file.read()
 ip_list = file_handle(content)
 port_list = []
+j=0
 for i in ip_list:
-    port_list.append(OpenPort(ip_list(i),i))
-    
+  tmp = OpenPort(i,str(j))
+  port_list.append(tmp)
+  j+=1
+print (ip_list)
+# print("\n\n\n")   
 print(port_list)
 tmp_file.close()
